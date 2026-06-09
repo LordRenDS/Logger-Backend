36aa3adfe2ea9bdd8934dd50dfa58976f0efbe67
Ren <sergeysolovyov2016@gmail.com>
2026-06-09 19:40:17 +0200

    - create api resource wrappers for pc, process, and schedule

---
+++ app/Http/Resources/PcResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PcResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'unique_id' => $this->unique_id,
+            'name' => $this->name,
+            'last_seen_at' => $this->last_seen_at?->toIso8601String(),
+            'created_at' => $this->created_at?->toIso8601String(),
+            'updated_at' => $this->updated_at?->toIso8601String(),
+        ];
+    }
+}
